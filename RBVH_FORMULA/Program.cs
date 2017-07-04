d71f41518c1186627ec762ebb8ca276d4b4966cc
Thang Vuong <thangvuongbk@gmail.com>
2017-07-04 10:03:34 +0700

    The Initial Create

---
+++ RBVH_FORMULA/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RBVH_FORMULA
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new RBVH_FORMULA.View.DisplayLog());
+        }
+    }
+}
---
+++ RBVH_FORMULA/View/DisplayLog.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RBVH_FORMULA.View
+{
+    public partial class DisplayLog : Form
+    {
+        public DisplayLog()
+        {
+            InitializeComponent();
+        }
+    }
+}
